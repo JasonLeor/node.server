0d922b824ca1115378805617fedd137853765337
jason.su <slg372056737>
2017-09-28 17:15:15 +0800

    添加 mysql 数据库

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var query = require('../component/mysql')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+    let querystr = req.query;
+    let index = querystr.index;
+    let size = querystr.size;
+
+    query("select * from user limit " + index + "," + size, function (err, rows, fields) {
+        res.send(rows);
+    });
+
 });
 
 module.exports = router;
